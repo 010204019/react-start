2cecdd693bca77d095e4afabc45e6fa55b246f14
Fossil.Lin <37405615@qq.com>
2017-04-06 22:17:00 +0800

    增加作业列表

--- src/container/Activity/activitylist.jsx
+++ src/container/Activity/activitylist.jsx
@@ -10,22 +10,24 @@ function handleSelectChange(value) {
 
 
 const columns = [{
-    title: '姓名',
-    dataIndex: 'name',
+    title: '标题',
+    dataIndex: 'hwInfo.title',
     // sorter: true,
-    render: name => `${name.first} ${name.last}`,
+   // render: name => `${name.first} ${name.last}`,
+    width: '40%',
+}, {
+    title: '班级',
+    dataIndex: 'classInfo.className',
     width: '20%',
 }, {
-    title: '性别',
-    dataIndex: 'gender',
-    filters: [
-        { text: 'Male', value: 'male' },
-        { text: 'Female', value: 'female' },
-    ],
+    title: '科目',
+    dataIndex: 'hwInfo.teachingSubjects.name',
     width: '20%',
-}, {
-    title: '邮箱地址',
-    dataIndex: 'email',
+},
+{
+    title: '发布人',
+    dataIndex: 'hwInfo.teacherInfo.name',
+    width: '20%',
 }];
 //选择行选择函数
 const rowSelection = {
@@ -46,7 +48,9 @@ const rowSelection = {
 class ActivityList extends Component {
     state = {
         data: [],
-        pagination: {defaultPageSize:50},
+        pagination: {
+            defaultPageSize:10,
+            pageSize:10},
         loading: false,
     };
 
@@ -56,26 +60,32 @@ class ActivityList extends Component {
         this.setState({
             pagination: pager,
         });
+        alert(pagination.current)
         this.fetchUrl({
-            results: pagination.pageSize,
-            page: pagination.current,
-            sortField: sorter.field,
-            sortOrder: sorter.order,
-            ...filters,
+            "pageInfo.pageNo":pagination.current,
+            "pageInfo.page":pagination.current,
+            "pageInfo.rows":pagination.pageSize,
+            "pageInfo.pageSize":pagination.pageSize,
+            "hwInfo.releState":1,
         });
     }
-    fetchUrl = (params = {}) => {
+    fetchUrl = (params = {
+        "pageInfo.pageNo":1,
+        "pageInfo.page":1,
+        "pageInfo.pageSize":10,
+        "hwInfo.releState":1,
+
+    }) => {
         let _this = this;
         this.setState({ loading: true });
-        $http.getData('https://randomuser.me/api', {
-            results: 50,
-            ...params,
+        $http.getData('pchw_searchMyReleHwList.action', {
+            ...params
         }).then(function (response) {
             const pagination = { ..._this.pagination };
-            pagination.total = 200;
+            pagination.total = response.data.obj.total;
             _this.setState({
                 loading: false,
-                data: response.data.results,
+                data: response.data.obj.row,
                 pagination,
             });
         })
@@ -95,7 +105,7 @@ class ActivityList extends Component {
                 <div style={{marginTop:'15px'}}>
                       <Table columns={columns}
                 rowSelection={rowSelection}
-                rowKey={record => record.registered}
+                rowKey={record => record.id}
                 dataSource={this.state.data}
                 pagination={this.state.pagination}
                 loading={this.state.loading}
--- src/routes/routeconst.js
+++ src/routes/routeconst.js
@@ -1,4 +1,4 @@
-export const URL_LOGIN = "/logn";//登录
+export const URL_LOGIN = "/login";//登录
 export const URL_HOME_PAGE = "/app/homepage";//首页路径
 export const URL_ACTIVITY_LIST = "/app/activitys";//活动报名列表
 export const URL_ABOUT = "/app/about";//关于
